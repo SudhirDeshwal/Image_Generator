b23e2c5222d94e78fcfc759b48e93c958abbc145
SudhirDeshwal <50604228+SudhirDeshwal@users.noreply.github.com>
2020-05-24 22:37:39 -0400

    final 2 commit

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-// //challenge 1: your age in days
-// function ageinDays()
-// {
-// var birthyear = prompt('whats your birth year');
-// var ageinDayss = (2020 - birthyear)*365;
-//  var h1 = document.createElement('h1');
-//  var textarea = document.createTextNode("You are " + ageinDayss + "Days old");
-// h1.setAttribute("id", "ageinDays");
-// h1.appendChild(textarea);
-// document.getElementById('flex-box-result').appendChild(h1);
-// console.log(ageinDayss);
-// }
+//challenge 1: your age in days
+function ageinDays()
+{
+var birthyear = prompt('whats your birth year');
+var ageinDayss = (2020 - birthyear)*365;
+ var h1 = document.createElement('h1');
+ var textarea = document.createTextNode("You are " + ageinDayss + "Days old");
+h1.setAttribute("id", "ageinDays");
+h1.appendChild(textarea);
+document.getElementById('flex-box-result').appendChild(h1);
+console.log(ageinDayss);
+}
 
-// function reset(){
+function reset(){
 
-//     //document.getElementById('flex-box-result').innerHTML = "";
-//     //or
-//     document.getElementById('ageinDays').remove();
-// }
+    //document.getElementById('flex-box-result').innerHTML = "";
+    //or
+    document.getElementById('ageinDays').remove();
+}
 
 function generateCat() {
 
